3262a008fe2bbfdc1215410b90728b0d1c8ba572
Milhan Osman <milhanomarosman@gmail.com>
2023-06-21 15:00:58 +0300

    completed assigment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,12 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  
+  def show
+    #FIND CHEESE USING THE ID ROM THE URL
+    cheese = Cheese.find_by(id:params[:id])
+    #SEND A JSON RESPONSE USING THAT CHEESE BJECT
+    render json: cheese
+  end
 
 end
